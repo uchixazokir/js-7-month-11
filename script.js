84e62bed4e4d6a4e5dfd6e9d5aaaf6b50e40f9fa
uchixazokir <uchixaitaachi.71@gmail.com>
2025-10-08 13:42:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let h1 = document.querySelector(`h1`)
+let p = document.querySelector(`p`)
+
+new Typed(h1,{
+    strings : ['Text Gradients'],
+    typeSpeed : 100,
+    backSpeed:100,
+    loop: true
+
+})
+
+new Typed(p,{
+    strings:['Make your headlines stand out',''],
+    typeSpeed: 100,
+    backSpeed:100,
+    starDelay:1000,
+    loop: true
+
+})
